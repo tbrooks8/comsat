d712fd1f3224552c73e2f2b47218087055b54b27
Tim Brooks <tim@uncontended.net>
2015-12-02 19:32:04 -0600

    Bump kafka to most recent release. And fix related compilation issues

--- comsat-kafka/src/main/java/co/paralleluniverse/fibers/kafka/FiberKafkaProducer.java
+++ comsat-kafka/src/main/java/co/paralleluniverse/fibers/kafka/FiberKafkaProducer.java
@@ -14,7 +14,10 @@
 package co.paralleluniverse.fibers.kafka;
 
 import co.paralleluniverse.strands.SettableFuture;
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.PartitionInfo;
@@ -22,6 +25,7 @@ import org.apache.kafka.common.PartitionInfo;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class FiberKafkaProducer<K, V> implements Producer<K, V> {
 
@@ -51,6 +55,18 @@ public class FiberKafkaProducer<K, V> implements Producer<K, V> {
     @Override
     public Map<MetricName, ? extends Metric> metrics() {
         return producer.metrics();
+    }
+
+    @Override
+    public void close(long timeout, TimeUnit unit) {
+        producer.close(timeout, unit);
+
+    }
+
+    @Override
+    public void flush() {
+        // TODO: Handle blocking
+        producer.flush();
     }
 
     @Override
